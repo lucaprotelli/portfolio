818fe522dd26cf97c21eb1de42fe3cd51fd06a15
Luca Protelli <lucaprotelli23@icloud.com>
2024-11-11 12:18:36 +0100

    Remove unused image import from NotFound component

--- blog/app/not-found.tsx
+++ blog/app/not-found.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image"
-import myGif from "../app/public/images/phoyo.gif"
-
 export default function NotFound() {
     return (
         <section>
-            <Image src={myGif} alt="404" width={100} height={100} />
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
                 404 - Page Not Found
             </h1>
