3c87697f7c2e081ade899bb3d9189546586d88fc
Luca Protelli <lucaprotelli23@icloud.com>
2024-11-11 12:24:57 +0100

    Comment out Head component in layout for future reference

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -7,7 +7,7 @@ import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import Footer from "./components/footer"
 import { baseUrl } from "./sitemap"
-import Head from "next/head"
+// import Head from "next/head"
 
 export const metadata: Metadata = {
     metadataBase: new URL(baseUrl),
@@ -54,9 +54,9 @@ export default function RootLayout({
                 GeistMono.variable
             )}
         >
-            <Head>
+            {/* <Head>
                 <link rel="icon" href="/favicon.ico" />
-            </Head>
+            </Head> */}
             <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
                 <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
                     <Navbar />
